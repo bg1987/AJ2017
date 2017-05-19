0854dc62920e992ed467aa5c89a335e6725f29c9
Benny Gafanovich <bg1987@gmail.com>
2017-05-19 18:21:40 +0300

    Revert "removed custom actions, they dont work for some reason and we dont need them."
    This reverts commit ab8f7d5d018655abdad112f6c4aeeda7ee103b8f.

---
+++ Assets/AdventureJam2017/Scripts/CustomActions/CheckLastPanelInput.cs
@@ -0,0 +1,66 @@
+﻿/*
+ *
+ *	Adventure Creator
+ *	by Chris Burton, 2013-2016
+ *	
+ *	"ActionCheck.cs"
+ * 
+ *	This is an intermediate class for "checking" Actions,
+ *	that have TRUE and FALSE endings.
+ * 
+ */
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace AC
+{
+
+    /**
+	 * An Action subclass that allows for two different outcomes based on a boolean check.
+	 */
+    [System.Serializable]
+    public class CheckLastPanelInput : ActionCheck
+    {
+        public string input = "";
+        public string solution = "11233545";
+
+        public int constantID;
+        public int parameterID;
+
+        public static string totalinput = "";
+
+        /**
+		 * The default Constructor.
+		 */
+        public CheckLastPanelInput() : base()
+        {
+            category = ActionCategory.Custom;
+            title = "Check Panel Click";
+        }
+
+        /**
+		 * <summary>Works out which of the two outputs should be run when this Action is complete.</summary>
+		 * <returns>If True, then resultActionTrue will be used - otherwise resultActionFalse will be used</returns>
+		 */
+        public override bool CheckCondition()
+        {
+            input = AC.LocalVariables.GetStringValue(1);
+
+            totalinput += input;
+
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            if (solution.StartsWith(totalinput))
+                return true;
+
+            totalinput = "";
+            return false;
+        }
+    }
+}
---
+++ Assets/AdventureJam2017/Scripts/CustomActions/EnableDisableObjectAction.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace AC
+{
+
+    [System.Serializable]
+    public class EnableDisableCompon : AC.Action
+    {
+
+        // Declare variables here
+
+        public int parameterID = -1;
+        public int constantID = 0;
+        public bool enableStateObj;
+
+        public GameObject objectToAffect;
+        public bool newEnableState;
+
+        public EnableDisableCompon()
+        {
+            this.isDisplayed = true;
+            category = ActionCategory.Custom;
+            title = "Enable-Disable";
+            description = "This is a blank Action template.";
+        }
+
+        override public void AssignValues(List<ActionParameter> parameters)
+        {
+            objectToAffect = AssignFile(parameters, parameterID, constantID, objectToAffect);
+        }
+
+
+        override public float Run()
+        {
+            
+            if (objectToAffect)
+            {
+                objectToAffect.gameObject.SetActive(enableStateObj);
+            }
+
+            return 0f;
+
+        }
+
+#if UNITY_EDITOR
+
+        override public void ShowGUI(List<ActionParameter> parameters)
+        {
+            parameterID = Action.ChooseParameterGUI("Object to affect:", parameters, parameterID, ParameterType.GameObject);
+            if (parameterID >= 0)
+            {
+                constantID = 0;
+                objectToAffect = null;
+            }
+            else
+            {
+
+                objectToAffect = (GameObject)EditorGUILayout.ObjectField
+   ("Object to affect:", objectToAffect, typeof(GameObject), true);
+
+                constantID = FieldToID(objectToAffect, constantID);
+                objectToAffect = IDToField(objectToAffect, constantID, false);
+            }
+
+            enableStateObj = EditorGUILayout.Toggle("Enabled:", enableStateObj);
+
+            AfterRunningOption();
+        }
+        override public string SetLabel()
+        {
+            string labelAdd = "";
+            if (objectToAffect)
+                labelAdd = " (" + objectToAffect.name + ")";
+            return labelAdd;
+        }
+#endif
+    }
+}
