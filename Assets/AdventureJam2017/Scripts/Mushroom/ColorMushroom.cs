672df8883dcd7d9165bb80a0e218548de7293b7f
Benny Gafanovich <bg1987@gmail.com>
2017-08-05 19:32:51 +0300

    CYM mushroom riddle.

---
+++ Assets/AdventureJam2017/Scripts/Mushroom/ColorMushroom.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorMushroom : MonoBehaviour {
+
+    public Color mushroomColor;
+    public TargetMushroom target;
+    public void OnClick()
+    {
+        target.AddColor(mushroomColor);
+    }
+}
---
+++ Assets/AdventureJam2017/Scripts/Mushroom/TargetMushroom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMushroom : MonoBehaviour {
+
+    public SpriteRenderer Image;
+    public Color TargetColor;
+
+    public void AddColor(Color c)
+    {
+        Image.color -= c;
+    }
+
+    public void Reset()
+    {
+        Image.color = new Color(1, 1, 1, 1);
+    }
+
+    public void CheckMatch()
+    {
+        if(Image.color == TargetColor)
+        {
+            Debug.Log("WOOHOO");
+        }
+    }
+}
