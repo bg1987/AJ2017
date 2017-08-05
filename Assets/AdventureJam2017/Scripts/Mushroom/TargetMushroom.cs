08c75c93495e70615d5092c39a588af714b6b2e0
Benny Gafanovich <bg1987@gmail.com>
2017-08-05 23:49:32 +0300

    Color riddle improved, although still feels unnatural, need art input about the theory here.

--- Assets/AdventureJam2017/Scripts/Mushroom/TargetMushroom.cs
+++ Assets/AdventureJam2017/Scripts/Mushroom/TargetMushroom.cs
@@ -6,15 +6,41 @@ public class TargetMushroom : MonoBehaviour {
 
     public SpriteRenderer Image;
     public Color TargetColor;
+    public ParticleSystem particles;
+
+
+    private float minR;
+    private float minG;
+    private float minB;
+
+    public void Awake()
+    {
+        particles.Stop();
+    }
 
     public void AddColor(Color c)
     {
-        Image.color -= c;
+        SetMinimums(c);
+
+        var color = Image.color - c;
+        color.r = Mathf.Max(color.r, minR);
+        color.g = Mathf.Max(color.g, minG);
+        color.b = Mathf.Max(color.b, minB);
+
+        Image.color = color;
+        if (color == Color.white)
+            Reset();
+
+        CheckMatch();
     }
 
     public void Reset()
     {
         Image.color = new Color(1, 1, 1, 1);
+        minR = 0;
+        minG = 0;
+        minB = 0;
+        particles.Stop();
     }
 
     public void CheckMatch()
@@ -22,6 +48,20 @@ public class TargetMushroom : MonoBehaviour {
         if(Image.color == TargetColor)
         {
             Debug.Log("WOOHOO");
+            var parts = particles.main;
+            parts.startColor = TargetColor;
+            particles.Play();
         }
+        
     }
+
+    private void SetMinimums(Color c)
+    {
+        c = new Color32(85, 85, 85, 255) - c;
+        minR = Mathf.Min(c.r + minR, 1f);
+        minG = Mathf.Min(c.g + minG, 1f);
+        minB = Mathf.Min(c.b + minB, 1f);
+    }
+
+
 }
