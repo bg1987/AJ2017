1362fdde115870639f0ed9edb796efaa2299c2bf
Supreme <Supreme.main@gmail.com>
2017-08-07 09:36:49 +0300

    Changed the lily script to something coherent that works.

--- Assets/AdventureJam2017/Scripts/LilypadsPetal.cs
+++ Assets/AdventureJam2017/Scripts/LilypadsPetal.cs
@@ -1,28 +1,109 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum LilypadType
+enum LilyState
 {
-    Green,
-    Blue,
-    Red
+    Closed = 0,
+    Green = 1,
+    Purple = 2,
+    Yellow = 3
 }
 
 public class LilypadsPetal : MonoBehaviour {
 
-    public bool isEnabledOnLoad;
-    public LilypadType lilyType; 
+    [Header("Yellow Path")]
+    public LilypadsPetal nextYellowLily;
 
-	// Use this for initialization
-	void Start () {
+    [Header("Purple Path")]
+    public LilypadsPetal nextPurpleLily;
+
+    [Header("Return Path")]
+    public LilypadsPetal nextGreenLily;
+
+    [Space]
+    [Header("Refs")]
+    public GameObject ClosedState;
+    public GameObject OpenedState;
+    public GameObject Gem;
+
+    private LilyState currentState;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
     public void OpenLilypad()
     {
+        switch (StaffHeadColor.CurrentColorIndex)
+        {
+            case 0:
+                SwitchToNone();
+                break;
+            case 1:
+                SwitchToGreen();
+                break;
+            case 2:
+                SwitchToPurple();
+                break;
+            case 3:
+                SwitchToYellow();
+                break;
+            default:
+                break;
+        }
+    }
 
-        
-        
+    private void SwitchToYellow()
+    {
+        currentState = LilyState.Yellow;
+        ClosedState.SetActive(false);
+        OpenedState.SetActive(true);
+    }
+
+    private void SwitchToPurple()
+    {
+        currentState = LilyState.Purple;
+        ClosedState.SetActive(false);
+        OpenedState.SetActive(true);
+    }
+
+    /// <summary>
+    /// This is the return path back to start from every spot
+    /// </summary>
+    private void SwitchToGreen()
+    {
+        currentState = LilyState.Green;
+        ClosedState.SetActive(false);
+        OpenedState.SetActive(true);
+    }
+
+    private void SwitchToNone()
+    {
+        currentState = LilyState.Closed;
+        ClosedState.SetActive(true);
+        OpenedState.SetActive(false);
+    }
+
+    public bool HasMoreJumps
+    {
+        get
+        {
+            switch (currentState)
+            {
+                case LilyState.Closed:
+                    return false;
+                case LilyState.Green:
+                    return nextGreenLily != null;
+                case LilyState.Purple:
+                    return nextPurpleLily != null;
+                case LilyState.Yellow:
+                    return nextYellowLily != null;
+                default:
+                    return false;
+            }
+        }
     }
 }
