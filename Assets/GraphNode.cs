2a539e25d56b9be837ed39f4be5aa3ce9573b471
Benny Gafanovich <bg1987@gmail.com>
2017-08-22 23:49:35 +0300

    Test scene for graph (for water flowing puzzle)

---
+++ Assets/GraphNode.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphNode : MonoBehaviour {
+
+    public SpriteRenderer MySprite;
+    public GraphNode[] Nodes;
+    
+    public Color color;
+
+    public bool draw;
+    public bool clear;
+
+
+    public static bool showLines = true;
+    public static bool clearColor = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (draw)
+        {
+            clearColor = false;
+
+            draw = false;
+            ColorConnectedNodes(this.color);
+        }
+
+        if(showLines)
+        {
+            foreach (var n in Nodes)
+            {
+                Debug.DrawLine(transform.position, n.gameObject.transform.position);
+            }
+        }
+        
+        if(clear)
+        {
+            clear = false;
+            clearColor = true;
+        }
+
+        if(clearColor)
+        {
+            MySprite.color = Color.white;
+        }
+	}
+
+
+    void ColorConnectedNodes(Color c)
+    {
+        Debug.Log("Coloring " + gameObject.name);
+        MySprite.color = c;
+        foreach (var n in Nodes)
+        {
+            n.ColorConnectedNodes(c);
+        }
+    }
+}
---
+++ Assets/GraphSwitch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphSwitch : MonoBehaviour {
+    public GraphNode Target;
+    public GraphNode NodeA;
+    public GraphNode NodeB;
+
+    public bool flip;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(flip)
+        {
+            Flip();
+            flip = false;
+        }
+	}
+
+    private void Flip()
+    {
+        for (int i = 0; i < Target.Nodes.Length; i++)
+        {
+            if (Target.Nodes[i] == NodeA)
+            {
+                Target.Nodes[i] = NodeB;
+                return;
+            }
+            if(Target.Nodes[i] == NodeB)
+            {
+                Target.Nodes[i] = NodeA;
+                return;
+            }
+        }
+    }
+}
