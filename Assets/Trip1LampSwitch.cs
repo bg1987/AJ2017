300c8ccb0b0c5ed566c99b52ee2cc2fef7a31d50
Benny Gafanovich <bg1987@gmail.com>
2017-09-04 23:33:15 +0300

    Trip riddle 1

---
+++ Assets/Trip1LampSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trip1LampSwitch : MonoBehaviour {
+    public GameObject[] objects;
+    public int localVariableId;
+
+    public int currentIndex;
+	// Use this for initialization
+	void Start () {
+        currentIndex = AC.LocalVariables.GetIntegerValue(localVariableId);
+        SetObjectsToIndex();
+	}
+
+    void SetObjectsToIndex()
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            objects[i].SetActive(i == currentIndex);
+        }
+    }
+
+    public void Switch()
+    {
+        currentIndex = (currentIndex + 1) % objects.Length;
+
+        AC.LocalVariables.SetIntegerValue(localVariableId, currentIndex);
+
+        SetObjectsToIndex();
+    }
+	
+
+}
