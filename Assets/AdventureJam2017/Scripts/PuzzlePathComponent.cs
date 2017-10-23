468f158844ef1b5f219d84a1d54047ea845729ee
Supreme <Supreme.main@gmail.com>
2017-10-23 23:59:49 +0300

    Arranged the first puzzle connections in the statues scene. Added puzzle path component

---
+++ Assets/AdventureJam2017/Scripts/PuzzlePathComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzlePathComponent : MonoBehaviour {
+
+    public Action<PuzzlePathComponent> OnButtonClick;
+
+    public List<PuzzlePathComponent> connectedTo;
+
+    private void Awake()
+    {
+        var btn = GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        if (OnButtonClick != null)
+            OnButtonClick(this);
+    }
+}
