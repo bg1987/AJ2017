c2fc2d1f74bfcd784822ee0d36918ea6d2eaa501
Alexei Babin <Supreme.main@gmail.com>
2017-10-24 20:37:21 +0300

    Added puzzle parent script

---
+++ Assets/AdventureJam2017/Scripts/StatuesPuzzle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatuesPuzzle : MonoBehaviour {
+
+	List<PuzzlePathComponent> paths;
+
+	public PuzzlePathComponent currentLocation;
+
+	void Start() {
+		foreach (var path in paths) {
+			path.OnButtonClick += TryMoveToPos;
+		}
+	}
+
+	void TryMoveToPos (PuzzlePathComponent pathPoint)
+	{
+		List<string> scanned = new List<string>();
+		List<PuzzlePathComponent> completePath  = new List<PuzzlePathComponent>();
+
+		ScanForPath(currentLocation, pathPoint, completePath);
+	}
+
+	bool ScanForPath (PuzzlePathComponent currentLocation, PuzzlePathComponent pathPoint, List<PuzzlePathComponent> completePath)
+	{
+		throw new System.NotImplementedException ();
+	}
+}
