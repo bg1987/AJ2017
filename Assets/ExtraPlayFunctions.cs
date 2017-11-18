8411ea710756a903e65897b08ffea959dbf0245b
bg-pc\bg <bg1987@gmail.com>
2017-11-18 15:45:35 +0200

    falling from trip 2 to mushroom

---
+++ Assets/ExtraPlayFunctions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraPlayFunctions : MonoBehaviour {
+    
+
+    public void StopMovment()
+    {
+        var rig = GetComponent<Rigidbody2D>();
+        rig.velocity = Vector2.zero;
+        rig.angularDrag = 0;
+        rig.angularVelocity = 0;
+    }
+}
