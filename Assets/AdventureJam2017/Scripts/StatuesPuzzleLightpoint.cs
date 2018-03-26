6b435c8612f0b407762b7f07a68fc701c8382d90
Alexei Babin <alexei@moonactive.com>
2018-03-26 21:01:15 +0300

    Made some mother dragging progress

--- Assets/AdventureJam2017/Scripts/StatuesPuzzleLightpoint.cs
+++ Assets/AdventureJam2017/Scripts/StatuesPuzzleLightpoint.cs
@@ -3,52 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class StatuesPuzzleLightpoint : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+public enum StatueGemState
+{
+	idle,
+	moving,
+	returning
+}
 
-	public Vector3 startingPoint;
+public class StatuesPuzzleLightpoint : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
 	public PuzzlePathComponent initialPath;
 
 	PuzzlePathComponent currentPath;
 	PuzzlePathComponent destinationPath;
 	public float moveAmountPerSecond = 0.1f;
+	public float threshold = 0.2f;
 
-	bool isMoving;
+	StatueGemState gemState;
 	PointerEventData lastMouseData;
+
+	public void Awake() {
+		currentPath = initialPath;
+		gemState = StatueGemState.idle;
+	}
 	public void Update() {
 
-		
-
-		if(isMoving)
+		if(gemState == StatueGemState.moving)
 		{
 			transform.position = transform.position + ((destinationPath.transform.position - transform.position) * moveAmountPerSecond * Time.deltaTime);
 
-			if((destinationPath.transform.position - this.transform.position).magnitude < 0.2f )
+			if((destinationPath.transform.position - this.transform.position).magnitude < threshold )
 			{
 				SetNewPathPoint(destinationPath);
 			}
+			AC.KickStarter.player.Halt();
 		}
-		else
+		else if(gemState == StatueGemState.returning)
 		{
+			transform.position = transform.position + ((currentPath.transform.position - transform.position) * moveAmountPerSecond * Time.deltaTime);
 
+			if((destinationPath.transform.position - this.transform.position).magnitude < threshold )
+			{
+				gemState = StatueGemState.idle;
+			}
+			AC.KickStarter.player.Halt();
 		}
 	}
 
 	public void OnBeginDrag(PointerEventData eventData) {
 		lastMouseData = eventData;
-		isMoving = true;
-       	startingPoint = this.transform.position;
+		gemState = StatueGemState.moving;
+		SetNewPathPoint(currentPath);
+		AC.KickStarter.player.Halt();
     }
 
     public void OnDrag(PointerEventData eventData) {
 		lastMouseData = eventData;
-        this.transform.position = eventData.position;
+		AC.KickStarter.player.Halt();
     }
 
     public void OnEndDrag(PointerEventData eventData) {
 		lastMouseData = eventData;
-		isMoving = false;
-		this.transform.position = startingPoint;
+		gemState = StatueGemState.returning;
+		AC.KickStarter.player.Halt();
     }
 
 	public void SetNewPathPoint(PuzzlePathComponent newStartingPoint) {
@@ -63,5 +80,7 @@ public class StatuesPuzzleLightpoint : MonoBehaviour, IBeginDragHandler, IDragHa
 				destinationPath = point;
 			}
 		}
+
+		Debug.Log(string.Format("Setting new point :: {0} is the closest path. Current path: {1}", destinationPath.name, currentPath.name));
 	}
 }
